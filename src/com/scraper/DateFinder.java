1364cf75664359dcec6a7f6a7c790f279feb3ac0
HaythemSahbani <sh23077008@gmail.com>
2015-10-26 22:18:16 +0100

    update

--- src/com/scraper/DateFinder.java
+++ src/com/scraper/DateFinder.java
@@ -1,5 +1,7 @@
 package com.scraper;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,15 +19,37 @@ public class DateFinder {
     private static final String DATE_PATTERN =
             "(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[012])/((19|20)\\d\\d)";
 
-    public DateFinder(){
+    private static final String DATE_PATTERN_ATB ="(\\d.*\\d)";
+    private static final String ONLY_LETTERS_PATTERN ="[^\\W|\\d]+";
+
+    private static final ArrayList<String> monthList = new ArrayList<String>(
+            Arrays.asList("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"));
+
+    public DateFinder(){}
+
+    public String getDate(String date) {
         pattern = Pattern.compile(DATE_PATTERN);
-    }
-
-    public String getDate(String desc) {
-
-        String date ;
-        matcher = pattern.matcher(desc);
+        matcher = pattern.matcher(date);
         if(matcher.find()){date = matcher.group();}
+        else{ date="no date found";}
+        return date;
+    }
+    public String getATBDate(String date){
+
+        pattern = Pattern.compile(DATE_PATTERN_ATB);
+        matcher = pattern.matcher(date);
+        if(matcher.find()){
+            date = matcher.group();
+            pattern = Pattern.compile(ONLY_LETTERS_PATTERN);
+            matcher = pattern.matcher(date);
+                if(matcher.find()){
+                    String str = matcher.group();
+                    if (monthList.contains(str)){
+                        Integer monthIndex =  monthList.indexOf(str)+1;
+                        date = date.replaceAll(monthList.get(monthIndex-1), monthIndex.toString());
+                    }
+                }
+        }
         else{ date="no date found";}
         return date;
     }
